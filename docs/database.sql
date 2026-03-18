57dedb233a06ebea74c234555efc7c7dd1a0f7e5
busekayan <busekayan1507@gmail.com>
2026-03-18 18:02:08 +0300

    #12 create habits table with user relationship
    Created habits table and linked it to users table with foreign key constraint.

--- docs/database.sql
+++ docs/database.sql
@@ -5,3 +5,18 @@ CREATE TABLE users (
     password_hash NVARCHAR(255) NOT NULL,
     created_at DATETIME2 DEFAULT GETDATE()
 );
+
+--Habits Table
+CREATE TABLE habits (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    user_id INT NOT NULL,
+    name NVARCHAR(255) NOT NULL,
+    description NVARCHAR(500),
+    frequency_type NVARCHAR(50) NOT NULL, -- daily, weekly, custom
+    target_value INT,
+    created_at DATETIME2 DEFAULT GETDATE(),
+
+    CONSTRAINT FK_habits_user
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    ON DELETE CASCADE
+);
